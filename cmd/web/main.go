4761e05031f156e1c1ad4df45b66f98a4e8b1236
iVenomWeed <ivenomweed+github@gmail.com>
2025-04-23 13:33:53 +0530

    first chapter

---
+++ cmd/web/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+const cssVersion = "1"
+
+type config struct {
+	port int
+	env  string
+	api  string
+	db   struct {
+		dsn string
+	}
+	stripe struct {
+		secret string
+		key    string
+	}
+}
+
+type application struct {
+	config        config
+	infoLog       *log.Logger
+	errorLog      *log.Logger
+	templateCache map[string]*template.Template
+	version       string
+}
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+	app.infoLog.Printf(
+
+		"Starting HTTP server in %s mode on port %d",
+		app.config.env,
+		app.config.port,
+	)
+	return srv.ListenAndServe()
+}
+
+func main() {
+	log.SetFlags(log.Ldate | log.Lshortfile | log.LstdFlags)
+
+	var cfg config
+
+	flag.IntVar(
+		&cfg.port,
+		"port",
+		4000,
+		"Specify server port. { default: 4000 }",
+	)
+	flag.StringVar(
+		&cfg.env,
+		"env",
+		"dev",
+		"Specify deployment environment. { default: dev | prod }",
+	)
+	flag.StringVar(
+		&cfg.api,
+		"api",
+		"http://localhost:4001",
+		"Specify API url. { default: http://localhost:40001 }",
+	)
+	flag.Parse()
+
+	cfg.stripe.key = os.Getenv("STRIPE_KEY")
+	cfg.stripe.secret = os.Getenv("STRIPE_SECRET")
+
+	infoLog := log.New(
+		os.Stderr,
+		"INFO",
+		log.Ldate|log.Ltime|log.Lshortfile|log.LstdFlags,
+	)
+	errorLog := log.New(
+		os.Stderr,
+		"ERROR",
+		log.Ldate|log.Ltime|log.Lshortfile|log.LstdFlags,
+	)
+
+	tc := make(map[string]*template.Template)
+	app := &application{
+		config:        cfg,
+		infoLog:       infoLog,
+		errorLog:      errorLog,
+		templateCache: tc,
+		version:       version,
+	}
+
+	err := app.serve()
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
+}
